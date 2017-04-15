1aba771df0ee013e871ee3df479fb3f4a239d01a
Matt Bruton <the.bruton@gmail.com>
2017-04-14 20:33:19 -0500

    new layout header responsive and complete for current planned features

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  
+  isSearchDisabled: boolean = true;
   constructor() { }
 
   ngOnInit() {
