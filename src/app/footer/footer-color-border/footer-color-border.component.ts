da07c025f7e18139f52e573a1810f429fdf497ee
Matt Bruton <the.bruton@gmail.com>
2017-04-15 22:55:15 -0500

    cleaned up footer and header related components, refactored/removed junk code

---
+++ src/app/footer/footer-color-border/footer-color-border.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gb-footer-color-border',
+  templateUrl: './footer-color-border.component.html',
+  styleUrls: ['./footer-color-border.component.scss']
+})
+export class FooterColorBorderComponent {
+
+}
