ec9441bc523a949b3dfdf998fbbd96a58b5cca91
Matt Bruton <the.bruton@gmail.com>
2017-04-18 13:33:27 -0500

    restructuring application module in order to function better with angular routing, moved sidenav outside of the content component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,36 @@
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { ClarityModule } from 'clarity-angular';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
+import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
-import { CardDetailComponent } from './content/card-detail/card-detail.component';
-import { CardListComponent } from './content/card-list/card-list.component';
 import { ContentComponent } from './content/content.component';
-import { FooterColorBorderComponent } from './footer/footer-color-border/footer-color-border.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderBrandingComponent } from './header/header-branding/header-branding.component';
-import { HeaderColorBorderComponent } from './header/header-color-border/header-color-border.component';
 import { HeaderComponent } from './header/header.component';
-import { SidenavComponent } from './content/sidenav/sidenav.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 import { DataService } from 'app/shared/data.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardDetailComponent,
-    CardListComponent,
     ContentComponent,
-    FooterColorBorderComponent,
     FooterComponent,
     HeaderBrandingComponent,
-    HeaderColorBorderComponent,
     HeaderComponent,
-    SidenavComponent
+    SidenavComponent,
+    AboutComponent,
+    PageNotFoundComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     ClarityModule.forRoot(),
-    FlexLayoutModule,
     FormsModule,
     HttpModule
   ],
