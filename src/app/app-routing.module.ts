1457394269b99af6b51fae10b85b71036e4c5146
Matt Bruton <the.bruton@gmail.com>
2017-04-18 15:35:57 -0500

    added routes for application module: welcome and page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from "app/about/about.component";
+import { PageNotFoundComponent } from "app/page-not-found/page-not-found.component";
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: 'welcome', component: AboutComponent },
+  { path: '**', component: PageNotFoundComponent }
+  // {
+  //   path: '',
+  //   children: []
+  // },
 ];
 
 @NgModule({
