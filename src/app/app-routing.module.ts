a14ad9f9e4b1248b7cd127478bd33a0720a6fd3c
Matt Bruton <the.bruton@gmail.com>
2017-04-19 17:18:27 -0500

    modified scss structure/filenames slightly, changed references in components across the app to compensate for changes, rough working version of welcome component, need to refactor/cleanup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { AboutComponent } from 'app/about/about.component';
 import { PageNotFoundComponent } from 'app/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-  { path: 'welcome', component: AboutComponent },
+  { path: '', component: AboutComponent },
   { path: '**', component: PageNotFoundComponent }
   // {
   //   path: '',
