a0be700c5b5d47ca2b27ec368563f1fd982fd8d2
Matt Bruton <the.bruton@gmail.com>
2017-04-18 22:10:49 -0500

    separated UI related components into new shared module, imported module into application module

---
+++ src/app/shared/layout/content/content.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+// import { Card } from 'app/shared/model';
+
+@Component({
+  selector: 'gb-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.scss']
+})
+export class ContentComponent {
+  // @Input() selectedCard: Card;
+
+  // passCardToDetailView(card) {
+  //   this.selectedCard = card;
+  // }
+}
