136c9ed61974d0a93fe243b3085d69482eca4dba
Matt Bruton <the.bruton@gmail.com>
2017-04-20 17:33:21 -0500

    modified structure of app to give each module a directory for components in order to make it clear that each directory in app is based on it's module, not a mix of components and modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from 'app/about/about.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './components/about/about.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: AboutComponent },
